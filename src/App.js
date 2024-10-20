e2f1ad80f0007338d65c294820677bd2d07f3575
Hyunsu <hs9880@gmail.com>
2024-10-20 20:57:07 +0900

    feat: 초기 입력 빈문자열처리
    - 비동기 사용자 입력처리
    - 빈문자열 처리 내부 메서드 실행(추후 변경될수 있음)
    - 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+    isEmptyString(input){
+        return typeof input === "string" &&  input === ""
+    }
+  async run() {
+    const input  = await Console.readLineAsync()
+
+    try {      
+      if(this.isEmptyString(input)){
+        Console.print(`결과 : 0`);
+        return;
+      }
+
+      
+    } catch (error) {
+      Console.print(`[ERROR]: ${error.message}`);
+    }
+  }
 }
+
+
+
 
 export default App;
